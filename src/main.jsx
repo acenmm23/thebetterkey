e92b656520a4a5ddf7a111f6c62f13677d3f5621
acenmm23 <133246880+acenmm23@users.noreply.github.com>
2026-08-24 02:07:34 -0700

    Load scroll-driven interaction styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './product-refresh.css'
+import './scroll-effects.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
